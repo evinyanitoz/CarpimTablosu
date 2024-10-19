99405d9b49d3d4c0a2b8fb5b14da7f65814d393d
evinyanitoz <eyanitoz@gmail.com>
2024-10-19 16:52:40 +0300

    Add project files.

---
+++ CarpimTablosu/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CarpimTablosu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int toplam = 0;
+            Console.WriteLine("***********ÇARPIM TABLOSU*************");
+            for (int i = 1; i <= 10; i++)
+            {
+
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.Write(i + "*" + j + " : ");
+                    toplam = i * j;
+                    Console.WriteLine(toplam);
+                }
+             Console.WriteLine("*****************************************");
+            }
+        }
+
+            
+    }
+}
